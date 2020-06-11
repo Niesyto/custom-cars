96faba43ee371db7a85ea738d9ca3f767271b018
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2020-06-11 23:01:07 +0200

    Testing StylesProvider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
-import {SelectionPanelContainer} from './SelectionPanel.js';
-import {SummaryContainer} from './SummaryPanel.js';
+import { StylesProvider } from '@material-ui/core/styles';
+import { SelectionPanelContainer } from './SelectionPanel.js';
+import { SummaryContainer } from './SummaryPanel.js';
 
 
 function App() {
   return (
+    <StylesProvider>
       <div className="App">
         <header className="App-header" >
-          <Grid container justify="center" direction="row" className="Container" alignItems="center" alignContent="center" >        
-            <Grid item xs={12} lg={7} style={{maxWidth:"100%"}}>
+          <Grid container justify="center" direction="row" className="Container" alignItems="center" alignContent="center" >
+            <Grid item xs={12} lg={7} style={{ maxWidth: "100%" }}>
               <SelectionPanelContainer />
             </Grid>
-            <Grid item xs={12} lg={3} style={{maxWidth:"unset"}}>
+            <Grid item xs={12} lg={3} style={{ maxWidth: "unset" }}>
               <SummaryContainer />
-            </Grid>        
+            </Grid>
           </Grid>
         </header>
       </div>
+    </StylesProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { Provider } from 'react-redux';
 import store from './redux/store.js'
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
